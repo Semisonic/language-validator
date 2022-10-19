e5ffc909ff437ab7e777f227916eba31199badc8
Semisonic <Semisonic.GitHub@gmail.com>
2022-10-19 23:30:49 +0100

    config field annotations

--- api_server/config.py
+++ api_server/config.py
@@ -3,13 +3,13 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    ml_server_host: str = "http://127.0.0.1:5001"
-    ml_request_timeout: float = 10.0
-    ml_request_processor_count: int = 6
-    ml_request_retry_timeout: float = 1.0
-    sentence_queue_sync_response_threshold = 50
-    sentence_queue_rejection_threshold = 5000
-    sync_wait_timeout: float = 3.0
+    ml_server_host: str = "http://127.0.0.1:5001"  # self-explanatory
+    ml_request_timeout: float = 10.0               # time before requests to ML service timeout
+    ml_request_processor_count: int = 6            # number of async workers to send requests to ML service
+    ml_request_retry_timeout: float = 1.0          # waiting time before retries in case a request to ML service failed
+    sentence_queue_sync_response_threshold = 50    # if the amount of sentences in the queue is higher, validation will run in background
+    sentence_queue_rejection_threshold = 5000      # if the amount of sentences in the queue is higher, new posts will be rejected
+    sync_wait_timeout: float = 3.0                 # if validation takes more than this time, the request will return prematurely (BUGGY at the moment)
 
 
 @lru_cache()
--- ml_server/config.py
+++ ml_server/config.py
@@ -3,10 +3,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    word_dict_path: str = "ml_server/res/bad_words.txt"
-    ml_extra_timeout_mean: float = 1.3
-    ml_max_processing_time: int = 5
-    ml_no_service_probability: int = 5
+    word_dict_path: str = "ml_server/res/bad_words.txt"  # self-explanatory
+    ml_extra_timeout_mean: float = 1.3                   # the higher the number the longer the wait
+    ml_max_processing_time: int = 5                      # wait time cutoff
+    ml_no_service_probability: int = 5                   # probability (in percents) of producing a 503
 
 
 @lru_cache()
